39163e6380e97c5bcc5a0767b1046214d5c3ae6d
isinarman <isin.arman@gmail.com>
2025-01-30 22:39:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,8 +114,7 @@ async def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message, echo))
     application.add_error_handler(error_handler)
-    application.run_polling()
-    
+        
     set_webhook()
 
     await application.run_webhook(
