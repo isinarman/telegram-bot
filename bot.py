5178e96cc8bd8878f1cacc046f76b17a090117c6
isinarman <isin.arman@gmail.com>
2025-01-30 22:08:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,6 +66,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         f"Здравствуйте, {user_first_name}! Я бот агентства QazaqBots. Чем могу помочь?"
     )
 
+async def echo(update: Update, context):
+    await update.message.reply_text(update.message.text)
+
 # Обработчик текстовых сообщений
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
@@ -109,8 +112,9 @@ def main():
     application = Application.builder().token(TELEGRAM_TOKEN).build()
     
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message, echo))
     application.add_error_handler(error_handler)
+    application.run_polling()
     
     set_webhook()
 
