020f387b3e40c3fc0e382506639f66dc949c733d
isinarman <isin.arman@gmail.com>
2025-01-30 16:54:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -173,7 +173,7 @@ def setup_handlers(app: Application):
     )
     
     app.add_handler(conv_handler)
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_error_handler(error_handler)
 
 async def web_app(application: Application) -> web.Application:
