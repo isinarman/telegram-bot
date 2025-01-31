e8667de7886e3b553e8d6801cc9bc9f3729f403e
isinarman <isin.arman@gmail.com>
2025-01-31 15:07:50 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -122,5 +122,9 @@ async def main():
     )
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    try:
+        asyncio.run(main())
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(main())
